80819e9b8c5792b612161f09293a375a09f877ab
xXxNightBlazexXx <kyleakridge1268@gmail.com>
2024-06-24 19:23:22 -0400

    fixed some stuff

--- xxxnightblazexxx/src/App.js
+++ xxxnightblazexxx/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Careers from "./routes/Career";
 import Home from "./routes/Home";
 import Navbar from './Navbar';
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/careers" element={<Careers />} />
+        <Route path="/career" element={<Careers />} />
       </Routes>
     </>
   );
 }
+
 export default App;
--- xxxnightblazexxx/src/Navbar.js
+++ xxxnightblazexxx/src/Navbar.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+
 const Navbar =()=>{
       return (
             <div>
                   <Link to="/">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/careers">Careers</Link>
+                  <Link to="/career">Careers</Link>
             </div>
       )
 }
+
 export default Navbar;
