830776a9eca30bde25ff006cee6c750bd62ceb09
xXxNightBlazexXx <kyleakridge1268@gmail.com>
2024-06-24 19:33:22 -0400

    push

--- xxxnightblazexxx/src/App.js
+++ xxxnightblazexxx/src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Careers from "./routes/Career";
 import Home from "./routes/Home";
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/career" element={<Careers />} />
+        <Route path="/" element={<Home />} />  // Wrapped Home with JSX
+        <Route path="/career" element={<Careers />} /> // Wrapped Careers with JSX
       </Routes>
-    </Router>
+    </>
   );
 }
 
