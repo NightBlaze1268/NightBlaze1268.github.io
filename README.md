668eca2cfbdd731222038d270524e1cb7f5d2783
xXxNightBlazexXx <kyleakridge1268@gmail.com>
2024-06-24 20:01:21 -0400

    Changes made to the Navbar

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # xXxNightBlazexXx.github.io
 This repository serves as Kyle Akridge's user website for showcasing projects/skills/etc.
+
+
+## Test server
+run npm run start
+
+## Deploying to GitHub pages!!
+run 'npm run deploy'
+optionally specify a message 'npm run deploy -- -m "Deploying React app to GitHub Pages"'
+
+### The code is being deployed on the gh-pages branch
+### The source code is being stored on the main branch
--- xxxnightblazexxx/src/Navbar.js
+++ xxxnightblazexxx/src/Navbar.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
-const Navbar =()=>{
-      return (
-            <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/career">Careers</Link>
-            </div>
-      )
-}
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between">
+        <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+        <Link to="/career" className="text-white hover:text-gray-300">Careers</Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
