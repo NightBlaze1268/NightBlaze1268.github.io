eab1b9042e871c78b0e5bb6f0112278d5542654f
xXxNightBlazexXx <kyleakridge1268@gmail.com>
2024-06-27 18:22:14 -0400

    Finished the creation of the education page. Closes #1
    Added certs and achievements as downloads.
    Fixed coloring of education page.
    Updated the educationCard to remove unwanted code.

--- xxxnightblazexxx/src/components/educationCard.js
+++ xxxnightblazexxx/src/components/educationCard.js
@@ -3,25 +3,11 @@ import React from 'react';
 const posts = [
   {
     id: 1,
-    title: "Boost your conversion rate",
     href: "#",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
   },
-  // More posts...
 ];
 
-export default function EducationCard( { id, cardTitle, cardDescription, cardDate, cardCategory } ) {
+export default function EducationCard( { id, cardTitle, cardSubtitle, cardDescription, cardDate, cardCategory, certFileUrl, certName } ) {
   return (  
     <div>
       {posts.map((post) => (
@@ -33,7 +19,7 @@ export default function EducationCard( { id, cardTitle, cardDescription, cardDat
               {cardDate}
             </time>
             <a
-              href={post.category.href}
+              href={post.href}
               className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
               {cardCategory}
             </a>
@@ -45,26 +31,24 @@ export default function EducationCard( { id, cardTitle, cardDescription, cardDat
                 {cardTitle}
               </a>
             </h3>
-            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+            <h4 className="mt-2 text-base font-medium leading-6 text-gray-900 group-hover-text-gray-600 dark:text-gray-100">
+              { cardSubtitle }
+            </h4>
+            <p className="mt-5 line-clamp-4 text-sm leading-6 text-gray-600">
               {cardDescription}
             </p>
           </div>
-          <div className="relative mt-8 flex items-center gap-x-4">
-            <img
-              src={post.author.imageUrl}
-              alt=""
-              className="h-10 w-10 rounded-full bg-gray-50"
-            />
-            <div className="text-sm leading-6">
-              <p className="font-semibold text-gray-900">
-                <a href={post.author.href}>
-                  <span className="absolute inset-0" />
-                  {post.author.name}
-                </a>
-              </p>
-              <p className="text-gray-600">{post.author.role}</p>
+          {certName && certFileUrl && (
+            <div className="relative mt-8 flex flex-col gap-y-2">
+              <a
+                href={certFileUrl}
+                download
+                className="text-blue-500 hover:text-blue-700"
+              >
+                Download {certName}
+              </a>
             </div>
-          </div>
+          )}
         </article>
       ))}
     </div>
